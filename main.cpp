f68321cc1d334d217087518a4f4e52e8e96526e6
Verbkin Mike <verbkinm@yandex.ru>
2021-09-22 13:31:19 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,19 +11,5 @@ int main(int argc, char *argv[])
 
     w.showMaximized();
 
-    for (int i = 0; i < 5; i++)
-    {
-        qsrand(QDateTime::currentMSecsSinceEpoch());
-        float t1 = qrand() % ((70 + 1) - 30) + 30;
-        float t2 = qrand() % ((70 + 1) - 30) + 30;
-        float t3 = qrand() % ((70 + 1) - 30) + 30;
-        float t4 = qrand() % ((70 + 1) - 30) + 30;
-        std::array<float, 4> arr{ t1, t2, t3, t4 };
-        w.addPoints(QDateTime::currentDateTime(), arr);
-
-        QThread::sleep(5);
-    }
-
-
     return a.exec();
 }
